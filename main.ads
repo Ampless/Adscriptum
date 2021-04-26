af0b52eb76949381adb77b73b6d63b7a1858695e
chrissx <chrissx@chrissx.de>
2021-04-26 13:00:00 +0200

    we changed the defn

--- main.ads
+++ main.ads
@@ -1,7 +1,7 @@
 (defnative QApp "QApplication::QApplication(int, char **)")
 (defnative MainWidget "MainWidget::MainWidget()")
 
-(defn main :export args
+(defn :export main args
   (let app (QApp (len args) args))
   (let widget (MainWidget))
   (widget "show()")
